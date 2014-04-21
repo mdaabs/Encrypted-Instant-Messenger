9b8061e13f3785dd75d0c0085738b7a1a3c88b14
Nicole Cote <nicole.cote15@gmail.com>
2014-04-20 20:43:42 -0400

    Renamed the files

---
+++ Client/sendlogin.h
@@ -0,0 +1,17 @@
+/*******************************************************************
+*
+* CS 410
+* Team 7, #2scoops
+* 
+* Header file for sendLogin.cpp
+*
+*******************************************************************/
+
+#ifndef SENDLOGIN_H
+#define SENDLOGIN_H
+
+void sendlogin();
+
+#endif
+
+
