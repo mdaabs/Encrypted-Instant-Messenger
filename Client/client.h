f4ecbf7d6bc6b69215c2bd3f3ea516f8c61ffa7e
Nicole Cote <nicole.cote15@gmail.com>
2014-04-20 23:37:31 -0400

    Header file for functions found in client.cpp

---
+++ Client/client.h
@@ -0,0 +1,19 @@
+/********************************************************
+*
+* CS 410
+* Encrypted Instant Messenger
+* Team 7, #2scoops
+*
+* File: client.h
+*
+* Header file for functions found in client.cpp
+*
+********************************************************/
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+// Send username and password to the server
+void sendlogin(string username, string password, int server);
+
+#endif
