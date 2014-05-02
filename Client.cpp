e1c5c472c8c1dc7396d24a952a42302af2d8c718
abf617 <abf617@gmail.com>
2014-05-01 23:07:09 -0400

    added client functions

--- Client.cpp
+++ Client.cpp
@@ -52,8 +52,16 @@ messagetype ReceiveAction(){
 
 
 
-
 }*/
+
+bool SendMesage(std::string message, int* socket){
+
+	if(write(*socket, message.c_str(), sizeof(message))<0)
+		return true;
+	else
+		return false;
+}
+
 
 //int main (int argc, char* argv[])
 int *ConnectClient(std::string IP, std::string port)
