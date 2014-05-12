7d09a108de87415d75b267dbc9b09ae8bc2fa91f
mdaabs <mdaabs@gmail.com>
2014-05-12 17:23:08 -0400

    update

--- b64.h
+++ b64.h
@@ -10,3 +10,4 @@ char* base64Encode(const unsigned char *buffer, const size_t length);
 int base64Decode(const char *b64message, const size_t length, unsigned char **buffer);
 int calcDecodeLength(const char *b64input, const size_t length);
 
+ 
