3e6d30b4bd44013b062ce165bf0d95df5cbdeac4
mdaabs <mdaabs@gmail.com>
2014-05-14 22:42:13 -0400

    rsa remnant cleanup

--- Encryption.h
+++ Encryption.h
@@ -20,15 +20,9 @@
 class Encryption {
 public:
 	Encryption();
-	Encryption(unsigned char *remotePubKey, size_t remotePubKeyLen);
     ~Encryption();
     int EncryptAes(const unsigned char *msg, size_t msgLen, unsigned char **encMsg, unsigned char *aesKey, unsigned char *aesIV);
     int DecryptAes(unsigned char *encMsg, size_t encMsgLen, unsigned char **decMsg, unsigned char *aesKey, unsigned char *aesIV);
-    int writeKeyToFile(FILE *fd, int key);
-    int getRemotePubKey(unsigned char **pubKey);
-    int setRemotePubKey(unsigned char *pubKey, size_t pubKeyLen);
-    int getLocalPubKey(unsigned char **pubKey);
-    int getLocalPrivateKey(unsigned char **privateKey);
     int getAesKey(unsigned char **aesKey);
     int setAesKey(unsigned char *aesKey, size_t aesKeyLen);
     int getAesIV(unsigned char **aesIV);
@@ -36,13 +30,8 @@ public:
     std::string printKey();
     std::string printIV();
 private:
-	static EVP_PKEY *localKeyPair;
-	EVP_PKEY *remotePubKey;
-
-	EVP_CIPHER_CTX *EncryptRsaCtx;
 	EVP_CIPHER_CTX *EncryptAesCtx;
 
-	EVP_CIPHER_CTX *DecryptRsaCtx;
 	EVP_CIPHER_CTX *DecryptAesCtx;
 
 	unsigned char *aesKey;
